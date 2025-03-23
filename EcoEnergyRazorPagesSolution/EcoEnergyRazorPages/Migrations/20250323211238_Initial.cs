88e0c9e00d9ee89420760cb2a2d6bbaeda0fbc16
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-23 22:16:57 +0100

    Initial Migration

---
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Migrations/20250323211238_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoEnergyRazorPages.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Simulations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SysTypeId = table.Column<int>(type: "int", nullable: false),
+                    SysType = table.Column<int>(type: "int", nullable: false),
+                    SunHours = table.Column<double>(type: "float", nullable: false),
+                    WindVelocity = table.Column<double>(type: "float", nullable: false),
+                    WaterFlow = table.Column<double>(type: "float", nullable: false),
+                    Ratio = table.Column<double>(type: "float", nullable: false),
+                    EnergyGen = table.Column<double>(type: "float", nullable: false),
+                    KWHCost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    KWHPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Simulations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Simulations");
+        }
+    }
+}
