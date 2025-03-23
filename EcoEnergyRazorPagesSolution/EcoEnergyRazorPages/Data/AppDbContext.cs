14c30fb8699032cc53baf6ab01c2d569411ad541
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-23 18:14:45 +0100

    created DbContext

---
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EcoEnergyRazorPages.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoEnergyRazorPages.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Simulation> Simulations { get; set; }
+        public DbSet<WaterConsumption> WaterConsumptions { get; set; }
+        public DbSet<EnergyIndicator> EnergyIndicators { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
