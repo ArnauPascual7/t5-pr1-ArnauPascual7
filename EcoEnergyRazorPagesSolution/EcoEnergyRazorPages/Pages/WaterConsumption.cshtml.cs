b5a8a19e4eedd443bac088dba20567b4e6f72885
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-03-23 18:35:08 +0100

    Deleted Unused Code

--- EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/WaterConsumption.cshtml.cs
+++ EcoEnergyRazorPagesSolution/EcoEnergyRazorPages/Pages/WaterConsumption.cshtml.cs
@@ -16,10 +16,6 @@ namespace EcoEnergyRazorPages.Pages
     {
         public string? MsgFileError { get; set; }
         public List<WaterConsumption> WaterConsumptions { get; set; } = new List<WaterConsumption>();
-        public List<WaterConsumption> RegionsWithMoreWaterInLastYear { get; set; } = new List<WaterConsumption>();
-        public List<WaterConsumption> AverageWaterConsumptionByRegion { get; set; } = new List<WaterConsumption>();
-        public List<WaterConsumption> SusWaterConsumption { get; set; } = new List<WaterConsumption>();
-        public List<WaterConsumption> RegionsWithWaterConsumptionIncreasingTrendInLast5Years { get; set; } = new List<WaterConsumption>();
         public void OnGet()
         {
             const string MsgDataError = "Error de càrrega de dades";
@@ -27,8 +23,6 @@ namespace EcoEnergyRazorPages.Pages
             const string XmlFileName = "consum_aigua_cat_per_comarques.xml";
             const string CsvFilePath = @"ModelData\" + CsvFileName;
             const string XmlFilePath = @"ModelData\" + XmlFileName;
-            const int NumCountRegionsWithMoreWater = 10;
-            const int SusDigitsWaterConsumption = 6;
 
             if (SysIO.File.Exists(CsvFilePath) && SysIO.File.Exists(XmlFilePath))
             {
@@ -40,32 +34,6 @@ namespace EcoEnergyRazorPages.Pages
                     WaterConsumptions.AddRange(xmlWaterConsumptions);
                 }
                 WaterConsumptions.Sort(new WaterConsumptionComparer().YearRegionCompare);
-
-                RegionsWithMoreWaterInLastYear = Stats.CheckWaterConsumptionMostRecentYearList(WaterConsumptions);
-                RegionsWithMoreWaterInLastYear.Sort(new WaterConsumptionComparer().HouseholdConsumptionPerCapitaCompare);
-                RegionsWithMoreWaterInLastYear.Reverse();
-                if (RegionsWithMoreWaterInLastYear.Count >= NumCountRegionsWithMoreWater)
-                {
-                    RegionsWithMoreWaterInLastYear.RemoveRange(NumCountRegionsWithMoreWater - 1, RegionsWithMoreWaterInLastYear.Count - NumCountRegionsWithMoreWater);
-                }
-
-                AverageWaterConsumptionByRegion = WaterConsumptions
-                    .GroupBy(waterCons => waterCons.RegionCode)
-                    .Select(g => new WaterConsumption
-                    {
-                        RegionCode = g.Key,
-                        RegionName = g.First().RegionName,
-                        HouseholdConsumptionPerCapita = float.Round(g.Average(waterCons => waterCons.HouseholdConsumptionPerCapita), 2)
-                    })
-                    .OrderByDescending(waterCons => waterCons.HouseholdConsumptionPerCapita)
-                    .ToList();
-
-                SusWaterConsumption = Stats.CheckSusWaterConsumptionList(WaterConsumptions, SusDigitsWaterConsumption);
-                SusWaterConsumption.Sort(new WaterConsumptionComparer().YearRegionCompare);
-
-                RegionsWithWaterConsumptionIncreasingTrendInLast5Years = Stats.CheckWaterConsumptionLast5YearsList(WaterConsumptions);
-                RegionsWithWaterConsumptionIncreasingTrendInLast5Years.Sort(new WaterConsumptionComparer());
-                RegionsWithWaterConsumptionIncreasingTrendInLast5Years = Stats.CheckWaterConsumptionIncreasingTrendList(RegionsWithWaterConsumptionIncreasingTrendInLast5Years);
             }
             else
             {
